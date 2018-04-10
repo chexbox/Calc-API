ae5bc39d3b42e0699c84a4386d684cd2ceb68e34
chexbox <31783961+chexbox@users.noreply.github.com>
2018-04-10 16:47:06 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 
 function calc(in) {
-  var const = []; //constants
-  var oper = []; //operators
-  var vari = []; //variables
-  while {
+  var cons = []; //stores constants
+  var oper = []; //stores operators
+  var vari = []; //stores variables
+  var order = []; //stores order of consts, opers, and vars
+  var a = 0; //counter for which character you're on
+  var last; //stores what kind of charater was the last
+  while (a < in.length) { // loop for picking apart input
+    a += 1;
   };
 };
