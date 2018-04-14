ce7dbf8bd0f5ac2954cdc00af2137dcf568b29b0
chexbox <31783961+chexbox@users.noreply.github.com>
2018-04-14 10:36:20 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,9 +5,16 @@ function calc(in) {
   var vari = []; //stores variables
   var order = []; //stores order of consts, opers, and vars
   var a = 0; //counter for which character you're on
+  var length;
   var last; //stores what kind of charater was the last
   while (a < in.length) { // loop for picking apart input
     if (in[a] == "0" || in[a] == "1" || in[a] == "2" || in[a] == "3" || in[a] == "4" || in[a] == "5" || in[a] == "6" || in[a] == "7" || in[a] == "8" || in[a] == "9") {
+      if (last == "number") {
+        length += 1;
+      } else {
+        length = 0;
+        last = "number";
+      };
     };
     a += 1;
   };
