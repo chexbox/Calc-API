6efa8d7d3a47e09473532408de81d7339fe45cb3
chexbox <31783961+chexbox@users.noreply.github.com>
2018-04-18 09:41:29 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function calc(in) {
-  var usablecons = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]   
+  var usablecons = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+  var usableoper = ["+", "-", "*", "/", "(", ")", "|", "=", "^", ""];
   var cons = []; //stores constants
   var oper = []; //stores operators
   var vari = []; //stores variables
