e62e437d5ddee8c503c524ea15d6628ebfc86b55
chexbox <31783961+chexbox@users.noreply.github.com>
2018-04-22 16:35:19 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,39 @@
-function calc(in) {
-  var usablecons = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
-  var usableoper = ["+", "-", "*", "/", "(", ")", "|", "=", "^", ""];
-  var cons = []; //stores constants
-  var oper = []; //stores operators
-  var vari = []; //stores variables
-  var order = []; //stores order of consts, opers, and vars
+// slightly based off https://github.com/joker314/bidmas-solver/
+function split(input) {
+  var cons = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+  var oper = ["+", "-", "*", "/", "(", ")", "|", "=", "^", ""];
+  var variable = ["x", "y"]
+  var out = {};
   var a = 0; //counter for which character you're on
+  var index = 0;
   var length;
   var last; //stores what kind of charater was the last
-  while (a < in.length) { // loop for picking apart input
-    if (usablecons.indexOf(in[a]) > -1) {
+  while (a < input.length) { // loop for picking apart input
+    if (cons.indexOf(input[a]) > -1) {
       if (last == "number") {
         length += 1;
       } else {
+        out[index] = {"value": input.substring(a - ( length + 1), a - 1)), "type": last};
         if (last == "operator") {
-          oper.push(a.substring(a - ( length + 1), a - 1));
-          order.push("oper")
+          out[index].p = "0"
         };
-        if (last == "variable") {
-          oper.push(a.substring(a - ( length + 1), a - 1));
-          order.push("var")
-        };
+        index += 1;
         length = 0;
         last = "number";
       };
     };
-    if () {
-        
+    if (variable.indexOf(input[a]) > -1) {
+      if (last == "variable") {
+        length += 1;
+      } else {
+        out[index] = {"value": input.substring(a - ( length + 1), a - 1)), "type": last};
+        if (last == "operator") {
+          out[index].p = "0"
+        };
+        index += 1;
+        length = 0;
+        last = "variable";
+      };
     };
     a += 1;
   };
