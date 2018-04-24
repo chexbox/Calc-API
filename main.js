29da64df330e2015bd390eb36b0fe3dd37f97b25
chexbox <31783961+chexbox@users.noreply.github.com>
2018-04-23 19:09:42 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 // slightly based off https://github.com/joker314/bidmas-solver/
 function spl(input) {
-  var cons = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
-  var oper = ["+", "-", "*", "/", "(", ")", "|", "=", "^", ""];
-  var variable = ["x", "y"]
-  var out = {};
-  var a = 0; //counter for which character you're on
-  var index = 0;
-  var length;
-  var last; //stores what kind of charater was the last
+  let cons = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+  let oper = ["+", "-", "*", "/", "(", ")", "|", "=", "^"];
+  let oper2 = ["sin", "cos", "tan", "floor", "ceiling", ""];
+  let variable = ["x", "y"]
+  let out = {};
+  let a = 0; //counter for which character you're on
+  let index = 0;
+  let length;
+  let last; //stores what kind of charater was the last
   while (a < input.length) { // loop for picking apart input
     if (cons.indexOf(input[a]) > -1) {
       if (last == "number") {
@@ -40,4 +41,15 @@ function spl(input) {
     out[index].p = "0"
   };
   return (out);
+};
+};
+function step(it) {
+  let oprs = {
+    "+": function(a, b) {return a+b},
+    "-": function(a, b) {return a-b},
+    "*": function(a, b) {return a*b},
+    "/": function(a, b) {return a/b},
+    "^": function(a, b) {return Math.pow(a+b)},
+  };
+  let index = 1;
 };
