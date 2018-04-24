6a688cffac4f1b6cbf1c05dc9d787e2b5988291a
chexbox <31783961+chexbox@users.noreply.github.com>
2018-04-24 10:44:46 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-// slightly based off https://github.com/joker314/bidmas-solver/
+// Based off https://github.com/joker314/bidmas-solver/
 function spl(input) {
   let cons = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
-  let oper = ["+", "-", "*", "/", "(", ")", "|", "=", "^"];
-  let oper2 = ["sin", "cos", "tan", "floor", "ceiling", ""];
+  let oper = ["+", "-", "*", "/", "^"];
+  let operp = {"+": 2, "-": 2, "*": 1, "/": 1, "^": 0}; // order of opeations
+  let oper2 = ["sin", "cos", "tan", "floor", "ceiling"];
   let variable = ["x", "y"]
   let out = {};
   let a = 0; //counter for which character you're on
